da781d352ad0225fa5b8020bb057f5a518f1d4a5
Shiva Poudel <shivapoudel@users.noreply.github.com>
2014-08-29 02:43:13 +0545

    Better syntax structure for potomo

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,10 +27,10 @@ module.exports = function( grunt ){
 
 		potomo: {
 			dist: {
-			options: {
-				poDel: false
-			},
-			files: [{
+				options: {
+					poDel: false
+				},
+				files: [{
 					expand: true,
 					cwd: 'languages/',
 					src: ['*.po'],
